323e57493ad4ad8f0d555a24c7ef4ffe2d8b2fb5
Tove <tovta309@student.liu.se>
2024-12-05 15:07:37 +0100

    Folders

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,6 @@
 import { Text, View } from "react-native";
+import React from 'react';
+
 
 export default function Index() {
   return (
@@ -9,7 +11,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Editaaaaa app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
